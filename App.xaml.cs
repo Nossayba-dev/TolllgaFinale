ecb56bd0c5cb8e9ae4a82fb342f4fd83950bd2a7
noussayba abbara <noussayba.abbara99@gmail.com>
2026-05-17 16:53:55 +0100

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace TolllgaFinale
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "TolllgaFinale" };
+        }
+    }
+}
